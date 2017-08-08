809a4989a619ead0d1bf3b44a161280a86920e66
Yan BO <2313567416@qq.com>
2017-08-08 11:41:31 +0800

    save

---
+++ 11视频播放&缓存/视频播放&缓存/视频播放&缓存/JUView.h
@@ -0,0 +1,13 @@
+//
+//  JUView.h
+//  视频播放&缓存
+//
+//  Created by 周磊 on 17/8/7.
+//  Copyright © 2017年 zhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JUView : UIView
+
+@end
---
+++ 11视频播放&缓存/视频播放&缓存/视频播放&缓存/JUView.m
@@ -0,0 +1,26 @@
+//
+//  JUView.m
+//  视频播放&缓存
+//
+//  Created by 周磊 on 17/8/7.
+//  Copyright © 2017年 zhl. All rights reserved.
+//
+
+#import "JUView.h"
+
+@implementation JUView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(void)didMoveToSuperview{
+    [super didMoveToSuperview];
+    
+}
+
+@end
